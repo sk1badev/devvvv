9c081de6ff3de78c0b0c41dad0d6fdb4ff38a4c6
sk1badev <99126936+sk1badev@users.noreply.github.com>
2022-05-16 18:31:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,9 @@
 # Author Name           : fabston                 #
 # File Name             : config.py               #
 # ----------------------------------------------- #
-
-# TradingView Example Alert Message:
-# {
-# "key":"9T2q394M92", "telegram":"-1001298977502", "discord":"789842349670960670/BFeBBrCt-w2Z9RJ2wlH6TWUjM5bJuC29aJaJ5OQv9sE6zCKY_AlOxxFwRURkgEl852s3", "msg":"Long #{{ticker}} at `{{close}}`"
-# }
-
+{"key":"9T2q394M92", "telegram":"-1001298977502", "discord":"789842349670960670/BFeBBrCt-w2Z9RJ2wlH6TWUjM5bJuC29aJaJ5OQv9sE6zCKY_AlOxxFwRURkgEl852s3", "msg":"Long #{{ticker}} at `{{close}}`"}
 sec_key = (
     "lrt22seccprft"  # Can be anything. Has to match with "key" in your TradingView alert message
-)
-
 # Telegram Settings
 send_telegram_alerts = False
 tg_token = "5093703648:AAFQcc_WEi1h2vDpU1fAevTs-THFMhRRZQs"  # Bot token. Get it from @Botfather
